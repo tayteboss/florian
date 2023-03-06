f72214dbfa75907999ed4af7c27f76bd87abe608
Tayte Boss <boss.tayte@gmail.com>
2023-03-06 11:49:57 +1100

    [feat] Adding facade

--- components/blocks/Facade/index.js
+++ components/blocks/Facade/index.js
@@ -17,28 +17,13 @@ const Model = ({ url }) => {
 
 const Lights = () => (
 	<>
-		<ambientLight intensity={0.3} />
-		<directionalLight
-			castShadow
-			position={[0, 0.5, 0]}
-			intensity={0.04}
-			shadow-mapSize-width={1024}
-			shadow-mapSize-height={1024}
-			shadow-camera-far={50}
-			shadow-camera-left={-10}
-			shadow-camera-right={10}
-			shadow-camera-top={10}
-			shadow-camera-bottom={-10}
-		/>
-		<pointLight position={[-10, 0, -20]} intensity={0.04} />
-		<pointLight position={[0, -10, 0]} intensity={0.04} />
-		<pointLight position={[10, 0, 0]} intensity={0.04} />
+		<ambientLight intensity={1} />
 	</>
 );
 
 const HTMLContent = ({ modelPath }) => {
 	const ref = useRef();
-	useFrame(() => (ref.current.rotation.y += 0.01));
+	useFrame(() => (ref.current.rotation.y += 0.001));
 	return (
 		<mesh ref={ref}>
 			<Model url={modelPath} />
@@ -46,54 +31,24 @@ const HTMLContent = ({ modelPath }) => {
 	);
 };
 
-const ImageWrapper = styled.div`
-	position: fixed;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-	height: 75%;
-	overflow: hidden;
-
-	@media ${(props) => props.theme.mediaBreakpoints.tabletPortrait} {
-		position: relative;
-		top: initial;
-		left: initial;
-		transform: translate(0);
-		height: 70vh;
-		height: 70svh;
-		margin-bottom: 70px;
-	}
-`;
-
-const Img = styled.img`
-	height: 100%;
-	width: 100%;
-	object-fit: contain;
-`;
-
 const Facade = () => (
-	<>
-		{true && (
-			<ImageWrapper>
-				<Img src="/icons/facade.svg" />
-			</ImageWrapper>
-		)}
-		{false && (
-			<FacadeWrapper>
-				<Canvas
-					concurrent="true"
-					colormanagement="true"
-					camera={{ position: [1.6, 0, 0], fov: 70 }}
-				>
-					<Lights />
-					<Suspense fallback={null}>
-						<HTMLContent modelPath="/facade/gitf-box.gltf" />
-					</Suspense>
-					<OrbitControls enableZoom={false} />
-				</Canvas>
-			</FacadeWrapper>
-		)}
-	</>
+	<FacadeWrapper>
+		<Canvas
+			concurrent="true"
+			colormanagement="true"
+			camera={{ position: [1, 0, 0], fov: 60 }}
+		>
+			<Lights />
+			<Suspense fallback={null}>
+				<HTMLContent modelPath="/facade/facade.gltf" />
+			</Suspense>
+			<OrbitControls
+				enableZoom={false}
+				minPolarAngle={1.5}
+				maxPolarAngle={1.8}
+			/>
+		</Canvas>
+	</FacadeWrapper>
 );
 
 export default Facade;
