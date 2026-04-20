ff3188aefd5bc9657dce7cbe914e0cbfe53dfd17
Tayte Boss <boss.tayte@gmail.com>
2026-04-21 09:52:04 +1000

    feat: Boolen for show bookings

--- components/blocks/HomeInfo/index.js
+++ components/blocks/HomeInfo/index.js
@@ -64,7 +64,7 @@ const HomeInfo = ({ options }) => {
 						inView={inView}
 						delay="450ms"
 					/> */}
-					{/* <FooterBooking inView={inView} /> */}
+					{options?.showDinnerBookings && <FooterBooking inView={inView} />}
 				</LayoutGrid>
 			</LayoutWrapper>
 		</HomeInfoWrapper>
